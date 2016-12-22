388d90250f4ab1806ed6c929151de65a85481515
lcrownover <lcrownover127@gmail.com>
2016-12-21 22:51:11 -0800

    begun analyze functions

--- analyze.py
+++ analyze.py
@@ -1,8 +1,22 @@
 #!/usr/bin/python3
 
+songs_list = []
+
 with open('./songs_played.txt', 'r') as f:
-    songs_list = []
     for line in f.readlines():
         songs_list.append(line.rstrip().split(" ~~ "))
 
-    print(songs_list)
+
+
+def get_song_play_count():
+    song_names = [i[0] for i in songs_list]
+
+    unique_songs = []
+    for song in song_names:
+        if song not in unique_songs:
+            unique_songs.append(song)
+
+    for i in unique_songs:
+        print(i)
+
+get_song_play_count()
