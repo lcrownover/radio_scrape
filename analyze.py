ddfe82d201645ec652f37594945b518dab8e97b3
lcrownover <lcrownover127@gmail.com>
2016-12-21 22:23:50 -0800

    no more regex, rstrip works

--- analyze.py
+++ analyze.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
 
-import re
-
 with open('./songs_played.txt', 'r') as f:
-
     songs_list = []
-    p = r"(\s\\n)|(\\n)"
-
     for line in f.readlines():
+        songs_list.append(line.rstrip().split(" ~~ "))
 
-        #trims newline characters from end of datetime
-        line = re.sub(p, '', str(line))
-        
-        songs_list.append(line.split(" ~~ "))
-        
     print(songs_list)
