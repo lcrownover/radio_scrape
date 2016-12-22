450e1efe1278c02d9f237748b44c18d29d9bf25c
lcrownover <lcrownover127@gmail.com>
2016-12-21 22:17:56 -0800

    regex to strip \n, still not working

--- analyze.py
+++ analyze.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
 
+import re
+
 with open('./songs_played.txt', 'r') as f:
 
     songs_list = []
+    p = r"(\s\\n)|(\\n)"
 
     for line in f.readlines():
+
+        #trims newline characters from end of datetime
+        line = re.sub(p, '', str(line))
+        
         songs_list.append(line.split(" ~~ "))
         
     print(songs_list)
