0c36661a198932b8b172d576cd8cabc634690f57
lcrownover <lcrownover127@gmail.com>
2016-12-30 17:09:02 -0800

    run

--- run.sh
+++ run.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
 cp ./cron.sh /etc/cron.daily
 database="./radio_songs.db"
 if [[ ! -f $target ]]; then 
     sqlite3 radio_songs.db < schema.sql
+fi
 nohup python3 main.py &>/dev/null &
