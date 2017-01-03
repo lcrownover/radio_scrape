270cd807c300b8d67d3dc8f3cffa3f1226665c36
Lucas Crownover <lcrownover127@gmail.com>
2017-01-03 12:55:51 -0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+
 cp -rf ./radio_scrape_cron.sh /etc/cron.daily
 chmod +x /etc/cron.daily/radio_scrape_cron.sh
+echo "Cron files copied"
+
+echo "Checking database..."
 database="./radio_songs.db"
-if [[ ! -f $database ]]; then 
+if [[ ! -f $database ]]; then
     sqlite3 radio_songs.db < schema.sql
+    chown lucasc:lucasc radio_songs.db
+    echo "Database not found, new one created."
 fi
-chown lucasc:lucasc radio_songs.db
+
+pid="$(ps axf | grep main.py | grep -v grep | awk '{print$1}')"
 nohup python3 main.py &>/dev/null &
+echo -e "Process started with pid $pid" 
