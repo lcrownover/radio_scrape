b34710800050377e8217460af1643392b51053cd
lcrownover <lcrownover127@gmail.com>
2017-01-04 14:54:21 -0800

    deleted old cron

--- radio_scrape_cron.sh
+++
@@ -1,5 +0,0 @@
-python3 /home/lucasc/git/radio_scrape/translate.py
-rm /home/lucasc/git/radio_scrape/songs_played.txt
-touch /home/lucasc/git/radio_scrape/songs_played.txt
-
-echo "successful entry" $(date) >> /home/lucasc/git/radio_scrape/cron-log.txt
