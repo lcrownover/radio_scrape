64a7b90816fea0e65af73c89dff16c812022f103
Lucas Crownover <lcrownuo@gmail.com>
2017-09-03 18:05:49 -0700

    fixed all the garbage

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
-#!/bin/bash
+ #!/bin/bash
 
 mkdir /opt/radio_scrape
-echo "made directory"
-#cp -rp ./source/* /opt/radio_scrape
-echo "did some stuff"
+cp -rp ./source/* /opt/radio_scrape
 bash /opt/radio_scrape/run.sh
 
 echo "radio_scrape installed to /opt/radio_scrape"
-
-echo "this is a change"
