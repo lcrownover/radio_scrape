204634f64d8a4c61382ae49a6315e5c2f9e4f069
Lucas Crownover <lcrownuo@gmail.com>
2017-09-03 18:00:05 -0700

    committing garbage to master

--- install.sh
+++ install.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
 mkdir /opt/radio_scrape
-cp -rp ./source/* /opt/radio_scrape
+echo "made directory"
+#cp -rp ./source/* /opt/radio_scrape
+echo "did some stuff"
 bash /opt/radio_scrape/run.sh
 
 echo "radio_scrape installed to /opt/radio_scrape"
 
+echo "this is a change"
